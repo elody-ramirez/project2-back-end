654cfca3bea5c4a04d4cf5dadf2d2c0b8c6f40c0
elody-ramirez <elody.ramirez@gmail.com>
2019-07-23 18:01:17 -0400

    Add limit to user entries to join table
    Added validation to user model and userplayers model to be able to
    limit a user to have a maximum of 3 players on their team

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 
 class User < ApplicationRecord
   include Authentication
+
   has_many :userplayers, dependent: :destroy
   has_many :players, through: :userplayers
+
+  validates :userplayers, length: { maximum: 3 }
 end
--- app/models/userplayer.rb
+++ app/models/userplayer.rb
@@ -3,4 +3,6 @@
 class Userplayer < ApplicationRecord
   belongs_to :user
   belongs_to :player
+
+  validates_associated :user
 end
